b6b1deb1f9f881afa107cf8043d163b4b7e57462
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-01-03 16:12:01 +0530

    Animating HSV colors with TweenAnimationBuilder

--- intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
+++ intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
@@ -20,8 +20,48 @@ class HSVColorSelector extends StatefulWidget {
 }
 
 class _HSVColorSelectorState extends State<HSVColorSelector> {
+  var _hue = 1.0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 0.0, end: _hue),
+          duration: Duration(milliseconds: 1500),
+          builder: (context, hue, child) {
+            final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
+            return Container(
+              width: 200,
+              height: 200,
+              color: hsvColor.toColor(),
+            );
+          },
+        ),
+        SizedBox(
+          height: 48,
+        ),
+        Container(
+          height: 30,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                for (var h = 0; h <= 360; h++)
+                  HSVColor.fromAHSV(1.0, h.toDouble(), 1.0, 1.0).toColor()
+              ],
+              stops: [
+                for (var h = 0; h <= 360; h++) h.toDouble() / 360,
+              ],
+            ),
+          ),
+        ),
+        Slider.adaptive(
+          value: _hue,
+          min: 0,
+          max: 360,
+          onChanged: (newHue) => setState(() => _hue = newHue),
+        )
+      ],
+    );
   }
 }
