24ea1c4054574902248ced3da84a7e11ab853beb
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-01-06 12:11:56 +0530

    Task completion UI tweaks

--- habit_tracker_flutter/lib/ui/tasks/animated_task.dart
+++ habit_tracker_flutter/lib/ui/tasks/animated_task.dart
@@ -56,22 +56,26 @@ class _AnimatedTaskState extends State<AnimatedTask>
 
   @override
   Widget build(BuildContext context) {
-    final themeData = AppTheme.of(context);
     return GestureDetector(
       onTapDown: _handleTapDown,
       onTapUp: _handleTapUp,
       child: AnimatedBuilder(
         animation: _curveAnimation,
         builder: (BuildContext context, Widget? child) {
+          final themeData = AppTheme.of(context);
+          final progress = _curveAnimation.value;
+          final hasCompleted = progress == 1;
+          final iconColor =
+              hasCompleted ? themeData.accentNegative : themeData.taskIcon;
           return Stack(
             children: [
               TaskCompletionRing(
-                progress: _curveAnimation.value,
+                progress: progress,
               ),
               Positioned.fill(
                 child: CenteredSvgIcon(
                   iconName: widget.iconName,
-                  color: themeData.taskIcon,
+                  color: iconColor,
                 ),
               ),
             ],
