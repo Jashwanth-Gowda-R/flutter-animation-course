6f3420e33a3e84462317c0c702490c8b268c2684
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-01-06 12:29:56 +0530

    Finished project and Wrap Up

--- habit_tracker_flutter/lib/ui/task/animated_task.dart
+++ habit_tracker_flutter/lib/ui/task/animated_task.dart
@@ -1,70 +1,81 @@
 import 'package:flutter/material.dart';
-
+import 'package:habit_tracker_flutter/constants/app_assets.dart';
 import 'package:habit_tracker_flutter/ui/common_widgets/centered_svg_icon.dart';
-import 'package:habit_tracker_flutter/ui/tasks/task_completion_ring.dart';
+import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class AnimatedTask extends StatefulWidget {
-  const AnimatedTask({
-    Key? key,
-    required this.iconName,
-  }) : super(key: key);
+  const AnimatedTask({Key? key, required this.iconName}) : super(key: key);
   final String iconName;
 
   @override
-  State<AnimatedTask> createState() => _AnimatedTaskState();
+  _AnimatedTaskState createState() => _AnimatedTaskState();
 }
 
 class _AnimatedTaskState extends State<AnimatedTask>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
   late final Animation<double> _curveAnimation;
-
+  bool _showCheckIcon = false;
   @override
   void initState() {
-    // implement initState
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: Duration(),
+      duration: Duration(milliseconds: 750),
     );
+    _animationController.addStatusListener(_checkStatusUpdates);
     _curveAnimation = _animationController.drive(
       CurveTween(curve: Curves.easeInOut),
     );
   }
 
-  void _handleTapDown(TapDownDetails details) {
-    if (_animationController.status != AnimationStatus.completed) {
-      _animationController.forward();
-    } else {
-      _animationController.reset();
+  @override
+  void dispose() {
+    _animationController.removeStatusListener(_checkStatusUpdates);
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _checkStatusUpdates(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      if (mounted) {
+        setState(() => _showCheckIcon = true);
+      }
+      Future.delayed(Duration(seconds: 1), () {
+        if (mounted) {
+          setState(() => _showCheckIcon = false);
+        }
+      });
     }
   }
 
-  void _handleTapUp(TapUpDetails details) {
+  void _handleTapDown(TapDownDetails details) {
+    if (_animationController.status != AnimationStatus.completed) {
+      _animationController.forward();
+    } else if (!_showCheckIcon) {
+      _animationController.value = 0.0;
+    }
+  }
+
+  void _handleTapCancel() {
     if (_animationController.status != AnimationStatus.completed) {
       _animationController.reverse();
     }
   }
 
   @override
-  void dispose() {
-    // implement dispose
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTapDown: _handleTapDown,
-      onTapUp: _handleTapUp,
+      onTapUp: (_) => _handleTapCancel(),
+      onTapCancel: _handleTapCancel,
       child: AnimatedBuilder(
         animation: _curveAnimation,
         builder: (BuildContext context, Widget? child) {
           final themeData = AppTheme.of(context);
           final progress = _curveAnimation.value;
-          final hasCompleted = progress == 1;
+          final hasCompleted = progress == 1.0;
           final iconColor =
               hasCompleted ? themeData.accentNegative : themeData.taskIcon;
           return Stack(
@@ -74,7 +85,9 @@ class _AnimatedTaskState extends State<AnimatedTask>
               ),
               Positioned.fill(
                 child: CenteredSvgIcon(
-                  iconName: widget.iconName,
+                  iconName: hasCompleted && _showCheckIcon
+                      ? AppAssets.check
+                      : widget.iconName,
                   color: iconColor,
                 ),
               ),
