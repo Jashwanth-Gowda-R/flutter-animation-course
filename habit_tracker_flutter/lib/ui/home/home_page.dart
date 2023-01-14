200f4251b3fee4612a91daa5b59166bfc1c5f4ea
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-01-14 20:23:01 +0530

    Using a GlobalKey to modify the state of a child widget

--- habit_tracker_flutter/lib/ui/home/home_page.dart
+++ habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -6,23 +6,48 @@ import 'package:habit_tracker_flutter/ui/home/page_flip_builder.dart';
 import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
 import 'package:hive/hive.dart';
 
+// class HomePage extends ConsumerWidget {
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final dataStore = ref.watch(dataStoreProvider);
+//     return PageFlipBuilder(
+//       frontBuilder: (_) => ValueListenableBuilder(
+//         valueListenable: dataStore.frontTasksListenable(),
+//         builder: (_, Box<Task> box, __) => TasksGridPage(
+//           tasks: box.values.toList(),
+//           onFlip: () {},
+//         ),
+//       ),
+//       backBuilder: (_) => ValueListenableBuilder(
+//         valueListenable: dataStore.backTasksListenable(),
+//         builder: (_, Box<Task> box, __) => TasksGridPage(
+//           tasks: box.values.toList(),
+//           onFlip: () {},
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class HomePage extends ConsumerWidget {
+  final _pageFlipKey = GlobalKey<PageFlipBuilderState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final dataStore = ref.watch(dataStoreProvider);
     return PageFlipBuilder(
+      key: _pageFlipKey,
       frontBuilder: (_) => ValueListenableBuilder(
         valueListenable: dataStore.frontTasksListenable(),
         builder: (_, Box<Task> box, __) => TasksGridPage(
           tasks: box.values.toList(),
-          onFlip: () {},
+          onFlip: () => _pageFlipKey.currentState?.flip(),
         ),
       ),
       backBuilder: (_) => ValueListenableBuilder(
         valueListenable: dataStore.backTasksListenable(),
         builder: (_, Box<Task> box, __) => TasksGridPage(
           tasks: box.values.toList(),
-          onFlip: () {},
+          onFlip: () => _pageFlipKey.currentState?.flip(),
         ),
       ),
     );
